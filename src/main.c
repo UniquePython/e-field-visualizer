0ab45527929fda2d96ff4527de5e2d9ff7cbb091
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2026-03-06 21:09:31 +0530

    Basic raylib setup

--- src/main.c
+++ src/main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
+#include <raylib.h>
 
+#define WIDTH 900
+#define HEIGHT 600
 
 int main(void)
 {
-	printf("Hello World!\n");
+	InitWindow(WIDTH, HEIGHT, "Electric Field Visualizer");
+	SetTargetFPS(GetMonitorRefreshRate(GetCurrentMonitor()));
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(BLACK);
+		EndDrawing();
+	}
+
+	CloseWindow();
+
 	return 0;
 }
