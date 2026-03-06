545b2afabaed868a64e278278662c869456bd1ea
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2026-03-06 22:03:08 +0530

    Compute electric field

--- src/main.c
+++ src/main.c
@@ -1,28 +1,71 @@
 #include <stdio.h>
 #include <raylib.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define WIDTH 900
-#define HEIGHT 600
+#define HEIGHT 900
 
 #define ROWS 20
 #define COLS 20
 
+#define FONT_SIZE 12
+
 #define CELL_W ((WIDTH) / (COLS))
 #define CELL_H ((HEIGHT) / (ROWS))
 
-float g_grid[ROWS][COLS];
+float g_field[ROWS][COLS], g_charges[ROWS][COLS];
 
-void zero_grid(void)
+void init_charges(void)
 {
-	memset(g_grid, 0, sizeof(g_grid));
+	for (int i = 0; i < ROWS; i++)
+		for (int j = 0; j < COLS; j++)
+			g_charges[i][j] = (rand() % 3) - 1;
+}
+
+void zero_field(void)
+{
+	memset(g_field, 0, sizeof(g_field));
+}
+
+void update_field(void)
+{
+	for (int self_y = 0; self_y < ROWS; self_y++)
+	{
+		for (int self_x = 0; self_x < COLS; self_x++)
+		{
+			for (int other_y = 0; other_y < ROWS; other_y++)
+			{
+				for (int other_x = 0; other_x < COLS; other_x++)
+				{
+					if (other_y == self_y && other_x == self_x)
+						continue;
+
+					float r_sq = (other_x - self_x) * (other_x - self_x) + (other_y - self_y) * (other_y - self_y);
+					g_field[self_y][self_x] += g_charges[other_y][other_x] / r_sq;
+				}
+			}
+		}
+	}
 }
 
 void draw_grid(void)
 {
 	for (int i = 0; i < ROWS; i++)
 		for (int j = 0; j < COLS; j++)
-			DrawRectangleLines(j * CELL_W, i * CELL_H, CELL_W, CELL_H, GRAY);
+		{
+			int x = j * CELL_W;
+			int y = i * CELL_H;
+
+			DrawRectangleLines(x, y, CELL_W, CELL_H, GRAY);
+
+			char buf[8] = {0};
+			snprintf(buf, sizeof(buf), "%.2f", g_field[i][j]);
+			int text_width = MeasureText(buf, FONT_SIZE);
+
+			DrawText(buf, x + ((CELL_W - text_width) / 2), y + (CELL_H / 2), FONT_SIZE, BLACK);
+		}
 }
 
 int main(void)
@@ -30,11 +73,15 @@ int main(void)
 	InitWindow(WIDTH, HEIGHT, "Electric Field Visualizer");
 	SetTargetFPS(GetMonitorRefreshRate(GetCurrentMonitor()));
 
+	srand(time(NULL));
+	init_charges();
+
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
-		zero_grid();
+		zero_field();
+		update_field();
 		draw_grid();
 		EndDrawing();
 	}
